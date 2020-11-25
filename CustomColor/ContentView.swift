56f51f76da44b9d12ed3bd85202872ee20a296fe
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-11-25 12:52:34 +0000

    first commit

--- CustomColor/ContentView.swift
+++ CustomColor/ContentView.swift
@@ -1,16 +1,29 @@
-//
-//  ContentView.swift
-//  CustomColor
-//
-//  Created by Tony Hajdini on 25/11/2020.
-//
-
+ 
 import SwiftUI
+
+extension Color {
+    static let teal = Color(red: 49 / 255, green: 163 / 255, blue: 159 / 255)
+    static let darkPink = Color(red: 208 / 255, green: 45 / 255, blue: 208 / 255)
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.darkPink, Color.teal]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            HStack {
+                Image(systemName: "swift")
+                    .foregroundColor(Color.darkPink)
+                    .font(.system(size: 35))
+                    .shadow(radius: 10)
+                Text("Custom Color in SwiftUI")
+                    .foregroundColor(Color.white)
+                    .fontWeight(.bold)
+            }
             .padding()
+            .background(Color.teal)
+            .cornerRadius(10)
+            .shadow(color: Color.white, radius: 20)
+        }.ignoresSafeArea()
     }
 }
 
